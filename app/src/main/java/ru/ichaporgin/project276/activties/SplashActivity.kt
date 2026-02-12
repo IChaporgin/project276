bb97bd438427b7502189417e965783b15db938b5
Igor Chaporgin <fellclub@yandex.ru>
2026-02-12 22:53:56 +0300

    refacroring images and created main_activity

--- app/src/main/java/ru/ichaporgin/project276/activties/SplashActivity.kt
+++ app/src/main/java/ru/ichaporgin/project276/activties/SplashActivity.kt
@@ -1,21 +1,24 @@
 package ru.ichaporgin.project276.activties
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import ru.ichaporgin.project276.R
+import ru.ichaporgin.project276.MainActivity
+import ru.ichaporgin.project276.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.splashImg.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
