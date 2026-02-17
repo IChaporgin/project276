3790fb6f4055557e1c4fff41fbee5fb94e477bbf
Igor Chaporgin <fellclub@yandex.ru>
2026-02-17 12:10:10 +0300

    cart activity ui

--- app/src/main/java/ru/ichaporgin/project276/Helper/TinyDB.java
+++ app/src/main/java/ru/ichaporgin/project276/Helper/TinyDB.java
@@ -88,7 +88,6 @@ public class TinyDB {
             bitmapFromPath = BitmapFactory.decodeFile(path);
 
         } catch (Exception e) {
-            // TODO: handle exception
             e.printStackTrace();
         }
 
